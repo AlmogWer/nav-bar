c996574e01add1e6d63e18002899acea3301fb56
Almog Wertzberger <Almogish@gmail.com>
2021-09-13 18:09:35 +0300

    adding functionality

---
+++ README.md
@@ -0,0 +1,41 @@
+<h1 align="center">Nav-Bar</h1>
+
+## Project Description
+
+React Nav bar with dynamic links and fully responsive
+
+## Built with
+
+- HTML
+- CSS
+- React
+
+# npm-start
+
+[npm start](https://docs.npmjs.com/cli/v7/commands/npm-start)
+
+```
+npm install
+npm install react-icons --save
+```
+
+```
+npm-start -g
+
+```
+
+## Links
+
+- [Repo](https://github.com/AlmogWer/nav-bar "Navbar Repo")
+- [Live](https://almogwer-navbar.netlify.app/ "Live View")
+
+## Screenshots
+
+![](img/Capture.PNG "Home Page")
+![](img/Capture2.PNG "Home Page - Smaller")
+
+## Author
+
+- [Almogwer](https://github.com/almogwer)
+- [Mail](mailto:Almogish@gmail.com?Subject=Hi% "Hi!")
+- [Linkedin](https://www.linkedin.com/in/almogwertzberger/)
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,37 +4,49 @@ import { links, social } from "./data";
 import logo from "./almogwer-logo.png";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = `0px`;
+    }
+  }, [showLinks]);
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { url, id, text } = link;
               return (
-                <li>
-                  <a href={url} key={id}>
-                    {text}
-                  </a>
+                <li key={id}>
+                  <a href={url}>{text}</a>
                 </li>
               );
             })}
           </ul>
         </div>
+
         <ul className="social-icons">
           {social.map((item) => {
             const { id, url, icon } = item;
             return (
-              <li>
-                <a href={url} key={id}>
-                  {icon}
-                </a>
+              <li key={id}>
+                <a href={url}>{icon}</a>
               </li>
             );
           })}
